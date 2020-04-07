15a38e618d5115fb76cb17146f171c8a12c55c8e
thunderrevolt <61190252+thunderrevolt@users.noreply.github.com>
2020-04-07 10:50:36 +0900

    index-new-post

--- app/uploaders/yonkoma_uploader.rb
+++ app/uploaders/yonkoma_uploader.rb
@@ -1,7 +1,8 @@
 class YonkomaUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
