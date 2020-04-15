9b578daa4a916e575a7d3d9b79e9e5135d4088c5
thunderrevolt <61190252+thunderrevolt@users.noreply.github.com>
2020-04-15 14:00:36 +0900

    change-s3

--- app/uploaders/yonkoma_uploader.rb
+++ app/uploaders/yonkoma_uploader.rb
@@ -50,15 +50,4 @@ class YonkomaUploader < CarrierWave::Uploader::Base
   else
     storage :fog
   end
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Amazon S3用の設定
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],  # S3に設定したリージョン。
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-    end
 end
